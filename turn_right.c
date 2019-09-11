2f80cf033e4e37de07bf26e818a06e8eb4f3e317
Kiefer Robotics <47461346+KieferRobotics@users.noreply.github.com>
2019-09-11 13:32:31 -0500

    Add files via upload

---
+++ turn_right.c
@@ -0,0 +1,17 @@
+#include <kipr/botball.h>
+#define r 0
+#define l 3
+
+void turn_right()
+{
+    cmpc(r);
+    cmpc(l);
+
+    while(gmpc(r) < -1075 && gmpc(l) > 1675)
+    {
+        mav(r,-500);
+        mav(l,500);
+    }
+
+
+}
